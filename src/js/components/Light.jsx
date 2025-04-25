4572b4204c95f860ef694413bdcbea70a884746e
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-04-25 01:51:27 +0000

    components and style of the traffic light

---
+++ src/js/components/Light.jsx
@@ -0,0 +1,13 @@
+// import {useState}
+
+export function Light(props) {
+    const colorClass = {
+        red: 'bg-danger',
+        yellow: 'bg-warning',
+        green: 'bg-success'
+    };
+
+    return (
+        <div className={`light rounded-circle ${colorClass[props.color]}`}></div>
+    );
+};
